17677caabaaf1a6c1c294bf9d150b791f436957d
somememoryspace <ali.afnan.mail@gmail.com>
2023-06-27 01:38:31 -0400

    create file function introd

--- src/file.rs
+++ src/file.rs
@@ -1,6 +1,28 @@
+use std::fs;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::path::Path;
+
 pub fn create_file(filepath: String) {
     let mut filepath_vec: Vec<String> = filepath.split("/").map(|s| s.to_string()).collect();
-    let filename = filepath_vec.remove(&filepath_vec.len()-1);
-    println!("{}", filename);
-    println!("{:?}", filepath_vec);
+    filepath_vec.pop();
+    let directory_tree_for_file = filepath_vec.iter().map(|x| x.to_string() + "/").collect::<String>();
+    println!("{}", &directory_tree_for_file);
+    let creation = fs::create_dir_all(&directory_tree_for_file);
+    let _creation = match creation {
+        Err(error) => panic!("panic! creating directory error: {:?}", error),
+        Ok(()) => {
+            println!("msg: directory created");
+            let init_file = File::create(&filepath);
+            let _result = match init_file {
+                Err(error) => panic!("panic! create file error: {:?}", error),
+                Ok(_file) => {
+                    match Path::new(&filepath).exists() {
+                        true => println!("msg: {} created", filepath),
+                        false => panic!("err: file create validation error"),
+                    }
+                },
+            }; 
+        },   
+    };
 }
